f29096249d97c6eb0648440a51cd2f6a2f8d5b0c
Ana Luiza <analuizagomeslima@hotmail.com>
2025-04-23 21:47:51 -0300

    Adicionando arquivos da pratica01 e pratica02

---
+++ praticas/pratica02/imprimir_nota_legal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("==============================\n");
+    printf("     N O T A    L E G A L     \n");
+    printf("==============================\n");
+    printf("Produto         Qtd Valor Unit  \n");
+    printf("%s %10.03i %10.1f\n", "Camiseta", 2, 39.99);
+    printf("%s %13.03i %10.1f\n", "Calca",1, 89.90);
+    printf("%s %7.03i %10.1f","Meia Social",3 ,19.99);
+    return 0;
+}
